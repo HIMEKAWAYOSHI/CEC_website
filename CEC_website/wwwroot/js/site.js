f9846ae9bc25281686a7e3c2e91451a36b3f26b1
HIMEKAWA <ranexhangin28@gmail.com>
2026-04-09 12:53:28 +0800

    added-src-each-view

--- CEC_website/wwwroot/js/site.js
+++ CEC_website/wwwroot/js/site.js
@@ -2,3 +2,75 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    var card = document.querySelector('.login-card');
+    if (!card) return;
+
+    // Start hidden
+    card.style.opacity = 0;
+    card.style.transform = 'translateY(30px)';
+
+
+    // Animate in
+    setTimeout(function () {
+        card.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
+        card.style.opacity = 1;
+        card.style.transform = 'translateY(0)';
+    }, 100);
+});
+
+//login page animation
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.index-card');
+    cards.forEach((card, i) => {
+        // Start hidden
+        card.style.opacity = 0;
+        card.style.transform = 'translateY(30px)';
+
+        setTimeout(() => {
+            card.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
+            card.style.opacity = 1;
+            card.style.transform = 'translateY(0)';
+        }, 200 + i * 200); // Stagger by 200ms
+    });
+
+
+
+  } )//index page animation
+
+document.addEventListener('DOMContentLoaded', function () {
+    const card = document.querySelector('.create-container');
+    if (!card) return;
+
+    // Start hidden
+    card.style.opacity = 0;
+    card.style.transform = 'translateY(10px)';
+    // Animate in
+    setTimeout(function () {
+        card.style.transition = 'opacity 0.9s ease, transform 0.9s ease';
+        card.style.opacity = 1;
+        card.style.transform = 'translateY(0)';
+    }, 100);
+}); //create page animation
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    const card = document.querySelector('.edit-animation');
+    if (!card) return;
+
+    // Start hidden
+    card.style.opacity = 0;
+    card.style.transform = 'translateY(30px)';
+    // Animate in
+      setTimeout(() => {
+        card.style.transition = 'opacity 0.6s ease, transform 0.6s ease';
+        card.style.opacity = 1;
+        card.style.transform = 'translateY(0)';
+    }, 100);
+});*/
+
+
+
+
+
+
